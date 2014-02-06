2eebbe9db3657bf4d0efeeebbd078550b59e40d4
masato <goodmoon.badmoon@gmail.com>
2014-02-06 16:44:05 +0900

    add uploader. I don't know why it works... I just copied and pasted from http://hirooka.pro/?p=2229

---
+++ app/controllers/Uploader.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json.Json
+
+object Uploader extends Controller {
+  def index = Action {
+    Ok(views.html.up())
+  }
+
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("file").map { file =>
+      import java.io.File
+      val filename = file.filename
+      val contentType = file.contentType
+      file.ref.moveTo(new File("/tmp", filename))
+      //Ok("File uploaded")
+      Ok(Json.toJson(Map("staus" -> "OK", "message" -> ("completed!")))).as("application/json")
+    }.getOrElse {
+      Redirect(routes.Uploader.index).flashing(
+        "error" -> "Missing file")
+    }
+  }
+}
