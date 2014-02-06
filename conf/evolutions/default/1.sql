752f649fa1c63df2316ae09622ee5c4a9d05eea3
masato <goodmoon.badmoon@gmail.com>
2014-02-06 16:13:39 +0900

    put a evolutions file. just create the apk table. postgresql does not allow the uppercase...

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,15 @@
+# apk scheme
+
+# --- !Ups
+
+CREATE TABLE apk (
+    id bigint NOT NULL primary key,
+    name varchar(255) NOT NULL,
+    apk_url varchar(255) NOT NULL,
+    icon_url varchar(255) NOT NULL,
+    package_name varchar(255) NOT NULL
+);
+
+# --- !Downs
+
+DROP TABLE apk;
